7271137c216234b8bc01c58a44beff72fcc6b24c
Anita Shinde <126587206+anushinde2002@users.noreply.github.com>
2024-06-13 15:45:28 +0530

    Add files via upload

---
+++ Comments/case_sencitive.php
@@ -0,0 +1,12 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ECHO "Marvellous Infosystems!<br>";
+echo "Marvellous Infosystems!<br>";
+EcHo "Marvellous Infosystems!<br>";
+?>
+
+</body>
+</html> 
---
+++ Comments/comment.php
@@ -0,0 +1,12 @@
+<?php
+echo "This program will illustrate all the ways in which we can do commenting"
+
+#Shell Style comment
+
+//C++ Style comment
+
+/*
+C Style Comment
+*/
+
+?>
