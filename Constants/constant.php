8c67a27c6a10fd511ae09e49b83d6dc6d570bb2c
Anita Shinde <126587206+anushinde2002@users.noreply.github.com>
2024-06-13 15:46:58 +0530

    Add files via upload

---
+++ Constants/constant.php
@@ -0,0 +1,13 @@
+<?php
+//User Defined constants
+// define(name, value, case-insensitive)
+
+define("MyConst","This is user defined constant <br>");
+
+echo(MyConst);
+//Built In constants:
+echo("We are in file: ".__FILE__."<br>");
+echo("We are on line: ".__LINE__."<br>");
+echo("We are in Function: ".__FUNCTION__."<br>");
+echo("We are in Class: ".__CLASS__."<br>");
+?>
